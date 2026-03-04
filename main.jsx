142aab70b8e33762c04035219a178c4e1cb75fb3
Sri Rangan <srirangan0603@gmail.com>
2026-03-04 20:51:34 +0530

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Login from './Login.jsx'
+import Home from './Home.jsx'
+import NotFound from './NotFound.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+const router= createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>,
+    errorElement:<NotFound/>
+  },
+
+  {
+    path:'/login',
+    element:<Login/>
+  }
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+
+  <RouterProvider router={router} />
+
+
+ 
+  </StrictMode>,
+)
+
+//  npm install react-router-dom 
