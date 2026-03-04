9377997532c3cf823ee91447a421103df7a5d4c3
Sri Rangan <srirangan0603@gmail.com>
2026-03-04 20:52:01 +0530

    Create NotFound.jsx

---
+++ NotFound.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+function NotFound(){
+    return (
+
+        <>
+        <h3>Error 404 found </h3>
+        <a className="btn btn-primary " href="/"> Home</a>
+        <Link  className="btn btn-danger" to="/" >Home</Link>
+        </>
+
+
+    );
+}
+
+export default NotFound;
